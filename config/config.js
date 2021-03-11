c3133f4531da5ef1b70247c7b6b6e8fd7d3d5acf
pacmanboss256 <pacmanboss972@gmail.com>
2021-03-10 21:32:58 -0600

    no

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (83186bd9/8cd74fc7)";
+Config.version = "0.11.2 (2f1de3d3/8cd74fc7)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
